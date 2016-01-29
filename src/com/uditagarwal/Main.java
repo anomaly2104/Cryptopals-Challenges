ec685a80ee661b6bca2f2763fb2a2c1074547631
Udit Agarwal <hello@uditagarwal.com>
2016-01-29 18:13:55 +0530

    Solve challenge 3 of set 1

--- src/com/uditagarwal/Main.java
+++ src/com/uditagarwal/Main.java
@@ -1,10 +1,9 @@
 package com.uditagarwal;
 
+import com.uditagarwal.Set1.c3;
+
 public class Main {
     public static void main(String[] args) {
-        String hexString1 = "1c0111001f010100061a024b53535009181c";
-        String hexString2 = "686974207468652062756c6c277320657965";
-
-        System.out.println("XOR Hex string: " + BaseConverter.binToHex(FixedXOR.binaryXOR(BaseConverter.hexToBin(hexString1), BaseConverter.hexToBin(hexString2))));
+        c3.solve();
     }
 }
---
+++ src/com/uditagarwal/Set1/c3.java
@@ -0,0 +1,31 @@
+package com.uditagarwal.Set1;
+
+import com.uditagarwal.BaseConverter;
+import com.uditagarwal.FixedXOR;
+
+/**
+ * Created by uditagarwal on 1/29/16.
+ */
+public class c3 {
+    static public String singleByteXORCipher(String textBin, String keyBin) {
+        int blockLength = 8;
+        assert textBin.length() % blockLength == 0;
+        String cipher = "";
+        for(int j = 0; j < textBin.length(); j += blockLength) {
+            String block = textBin.substring(j, j + blockLength);
+            String xor = FixedXOR.binaryXOR(block, keyBin);
+            char ch = (char) Integer.parseInt(xor, 2);
+            cipher += ch;
+        }
+        return cipher;
+    }
+
+    static public void solve() {
+        String cipherHex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+        String cipherBin = BaseConverter.hexToBin(cipherHex);
+        for (int i = 0; i < 255; i++ ) {
+            String keyBin = String.format("%08d",Integer.parseInt(Integer.toBinaryString(i)));
+            System.out.println((char) i + " : " + singleByteXORCipher(cipherBin, keyBin));
+        }
+    }
+}
